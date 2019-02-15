55b950632bd9bbe2626c2e5c8cb44093d1c6f549
chuangxinli <1358601985@qq.com>
2019-02-15 15:26:44 +0800

    0215 15:26

--- src/renderer/views/SignIn/SignIn.vue
+++ src/renderer/views/SignIn/SignIn.vue
@@ -243,7 +243,7 @@
 					}
           this.setSession('isSchoolUser', data.data.isSchoolUser)
 					this.jsonData = data.data
-					if(data.data.version != this.version){
+					if(data.data.version == this.version){
     	      this.newVersion = data.data.version
 						loadingInstance = Loading.service(loadOptions)
     	      axios.all([this.getSubjectAboutInfo(), this.getUnitAndSubUnit(), this.getUseMarkList()]).then(axios.spread( (SubjectAboutInfo, UnitAndSubUnit, UserMarkList) => {
@@ -252,14 +252,14 @@
                 this.$store.dispatch('CHANGE_VERSION', {version: this.newVersion})
                 this.$store.dispatch('UNIT_AND_SUBUINT', {unitAndSubUnit: UnitAndSubUnit.data.data})
                 this.$store.dispatch('SUBJECT_ABOUT_INFO', {subjectAboutInfo: SubjectAboutInfo.data.data})
-                /*writeFile(path.join(__dirname, '../../../main/json/subjectAboutInfo.json'), JSON.stringify(SubjectAboutInfo.data.data), err => {
+                writeFile(path.join(__dirname, '../../../main/json/subjectAboutInfo.json'), JSON.stringify({subjectAboutInfo: SubjectAboutInfo.data.data}), err => {
                   console.log(err)
                   return
                 })
-                writeFile(path.join(__dirname, '../../../main/json/unitAndSubUnit.json'), JSON.stringify(UnitAndSubUnit.data.data), err => {
+                writeFile(path.join(__dirname, '../../../main/json/unitAndSubUnit.json'), JSON.stringify({unitAndSubUnit: UnitAndSubUnit.data.data}), err => {
                   console.log(err)
                   return
-                })*/
+                })
                 this.$store.dispatch('USE_TAG_CHANGE', {userMarkList: UserMarkList.data.data})
 							}else{
                 Message({
